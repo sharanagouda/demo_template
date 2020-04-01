44de8e04237c35ec5b66eb76e549bd3eaefa10b7
sharanagouda <sharanu.mdl@gmail.com>
2020-04-01 15:01:42 +0530

    test(hero): carousel
    herocomponent
    
    nofooter

--- src/components/Carousel/stepIndicator/index.js
+++ src/components/Carousel/stepIndicator/index.js
@@ -1,26 +1,26 @@
-import Styles from "./StepIndicators.css";
+// import Styles from "./StepIndicators.css";
 
-export const StepIndicator = props => {
-  console.log("currentStep ", props.currentStep);
-  const indicators = [];
-  for (let index = 0; index < props.maxStep; index++) {
-    indicators.push(
-      <div
-        style={{
-          backgroundColor: "red",
-          height: "0.1vw",
-          margin: "1",
-          width: "3vw",
-          display: "inline-block"
-        }}
-        key={index}
-        className={
-          index === props.currentStep ? Styles.active : Styles.inactive
-        }
-        onClick={() => props.onClick(index)}
-      ></div>
-    );
-  }
+// export const StepIndicator = props => {
+//   console.log("currentStep ", props.currentStep);
+//   const indicators = [];
+//   for (let index = 0; index < props.maxStep; index++) {
+//     indicators.push(
+//       <div
+//         style={{
+//           backgroundColor: "red",
+//           height: "0.1vw",
+//           margin: "1",
+//           width: "3vw",
+//           display: "inline-block"
+//         }}
+//         key={index}
+//         className={
+//           index === props.currentStep ? Styles.active : Styles.inactive
+//         }
+//         onClick={() => props.onClick(index)}
+//       ></div>
+//     );
+//   }
 
-  return <div className={Styles.container}>{indicators}</div>;
-};
+//   return <div className={Styles.container}>{indicators}</div>;
+// };
